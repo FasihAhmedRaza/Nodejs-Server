ff88c94bb69f25d960bef04c2b327b9fbac92317
FasihAhmedRaza <thisfasih@gmail.com>
2023-07-18 11:43:21 +0500

    deploying to cyclic

---
+++ index.mjs
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('I m hello world  server!')
+})
+
+
+app.get('/fasih', (req, res) => {
+    res.send('I m fasih ahmed server!')
+  })
+  
+
+app.listen(port, () => {
+  console.log(`my server app listening on port ${port}`)
+})
